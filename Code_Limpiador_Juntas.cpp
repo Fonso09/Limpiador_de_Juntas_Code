8a208e56c936d6b7de70d913c8772635fee40978
Daniel Fonseca <fonsecafelipedaniel@gmail.com>
2024-11-17 00:20:13 -0500

    Implementación PWM para el motor y configuración lista para el servo

--- Code_Limpiador_Juntas.cpp
+++ Code_Limpiador_Juntas.cpp
@@ -6,12 +6,15 @@ void RCC_SETUP();
 void GPIO_SETUP();
 void USART_SETUP();
 int USART3_SendChar(int value);
+void TIM_Config();
 void carro_adelante();
 void carro_atras();
 void paro_emergencia();
+void Test_Serial();
 int in_adelante=0;
 int in_atras=0;
 int in_E_stop=0;
+int potencia=0;
 
 int main(){
     //Inicializar funciones y los setups 
@@ -19,12 +22,13 @@ int main(){
     GPIO_SETUP();
     SysTick_Init();	
     USART_SETUP();
-
-   
-
+    TIM_Config();
     while(1){
-		SysTick_Wait1ms(1500);	//pobjeto prueba para saber si la comunicacion serial esta sirviendo
-		USART3_SendChar('P');
+        Test_Serial();
+        TIM5->CCR1=5000; //PA0
+		TIM5->CCR2=1000; //PA1
+		TIM5->CCR3=1500; //PA2
+		TIM5->CCR4=2000; //PA3
         
         //Entradas 
         in_adelante= (GPIOF->IDR)& (1<<13);
@@ -33,7 +37,7 @@ int main(){
         GPIOB->ODR &= ~0xFFFF;
         GPIOE->ODR &= ~0xFFFF;
 
-        //Seleccionar funcion del carro, de manera manual con botones físicos:
+        //Seleccionar funcion del carro, de manera manual con botones fisicos:
         if(in_E_stop==(1<<13))
         {
             paro_emergencia();
@@ -45,17 +49,12 @@ int main(){
                 carro_atras();
             }
         }
-        
-
-
     }
-
-
 }
 
 void RCC_SETUP(){
 	RCC-> AHB1ENR |= 0xFF; //Activa los puertos de la A a la F
-    RCC->APB1ENR |= (1<<18); //habilita el USART3
+    RCC->APB1ENR |= (1<<18)|(1<<3); //habilita el USART3 y Habilita TIM5
 
 }
 void GPIO_SETUP(){
@@ -71,11 +70,29 @@ void GPIO_SETUP(){
     GPIOD->MODER |= 0xA0000;  //Pines a usar por el USART3 PD8(TX) y PD9(RX)
     GPIOD->AFR[1] |= 0x77; // Viendo el Datasheet se puede observar que el PD8 7 PD9 para USART3 se debe activar el AF7 y estan en la parte high del AFR
 
+    //GPIOC->MODER |= (2<<22)|(2<<20);  //Pines a usar por el USART3 PC10(TX) y PC11(RX)
+    //GPIOC->AFR[1] |= 0x7700; // Viendo el Datasheet se puede observar que el PC10 y PC11 para USART3 se debe activar el AF7 y estan en la parte high del AFR
+
+    GPIOA->MODER |=(2<<0)|(2<<2)|(2<<4)|(2<<6); //Modo alternante para el TIM5|PWM, PA0, PA1, PA2, PA3
+    GPIOA->AFR[0] |= 0x2222; //AF2 para el TIM5 de PA0, PA1, PA2 y PA3
+
 }
 void USART_SETUP(){
     USART3->BRR |= 0x683; //BAUD RATE 9600
     USART3->CR1 |= ((0x2D) | (0<<15)); //no hay oversampling del baud rate, se activa USART EN, Reciever y Transmiter EN, RX INTERRUPT EN
     NVIC_EnableIRQ(USART3_IRQn); //nombre en tabla del NVIC, interrupción
+}
+void TIM_Config(){
+    TIM5->PSC = 16-1;  // Para tener 50Hz
+    TIM5->ARR = 20000-1; //ARR
+    TIM5->DIER |= 0x1; //Habilita Interrupcion
+    TIM5->EGR |= 0x1; //Reinicio del conteo
+	TIM5->CCMR1|=0X6060; //Modo PWM Tim5  CH1 y 2 - Como salida 00 en CC1S
+	TIM5->CCMR2|=0X6060; //Modo PWM Tim5  CH3 y 4 - Como salida 00 en CC1S
+	TIM5->CCER|=(1<<0) | (1<<4)|(1<<8) | (1<<12);  //Habilitar salida de comparacion y captura
+    TIM5->CR1|=0X1; //Habilitar conteo
+	NVIC_EnableIRQ(TIM5_IRQn); //Por si queremos la interrupci?n que igual trabaja con el ARR
+        
 }
 void carro_adelante(){
     GPIOB->ODR &= ~0xFFFF;
@@ -97,6 +114,10 @@ int USART3_SendChar(int value){
    while(!(USART3->ISR & USART_ISR_TXE));
    return 0;
 }
+void Test_Serial(){
+	SysTick_Wait1ms(1500);	//pobjeto prueba para saber si la comunicacion serial esta sirviendo
+	USART3_SendChar('P');    
+}
 
 // funciones de los delays
 void SysTick_Init(void) {
@@ -115,6 +136,11 @@ void SysTick_Wait1ms(uint32_t delay) {
 }
 //interrupciones
 extern "C"{
+    void TIM5_IRQHandler(void){
+        TIM5->SR &=~(1UL<<0);
+        GPIOB->ODR^=(1<<0);   
+    }
+
     void USART3_IRQHandler(void){
         while(USART3->ISR & USART_ISR_RXNE){
             // lo que quiere que se haga cuando se reciba un dato por partde de la comunicación serial
